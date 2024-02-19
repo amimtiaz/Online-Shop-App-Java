393ab5ea99674d0053b34ab3331edd2b6afc65c0
amimtiaz <amemtiaj19@gmail.com>
2024-02-19 15:14:09 +0600

    notification bar design added

--- app/src/main/java/com/imtiaz_acedamy/online_shop_app_java/MainActivity.java
+++ app/src/main/java/com/imtiaz_acedamy/online_shop_app_java/MainActivity.java
@@ -3,12 +3,38 @@ package com.imtiaz_acedamy.online_shop_app_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+import com.imtiaz_acedamy.online_shop_app_java.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.editTextText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    char pressedChar = (char) event.getUnicodeChar();
+                    if (Character.isLetter(pressedChar)) {
+                        Toast.makeText(MainActivity.this, "Enter Key Pressed " + pressedChar, Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                return false;
+            }
+        });
+
     }
 }
